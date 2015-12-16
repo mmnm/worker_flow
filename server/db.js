63002e549f5cfb4c64238a92a01d8a61d8cc53e7
GregDungca <greg.dungca@gmail.com>
2015-12-16 12:10:36 -0800

    Added logging for database connection

--- server/db.js
+++ server/db.js
@@ -1,8 +1,20 @@
 var mysql = require('mysql');
 var config = require('./config');
 
-module.exports = mysql.createConnection({
+db = mysql.createConnection({
   user: 'root',
   password: '',
   database: 'wandoo'
 });
+
+db.connect(function(err) {
+  if (err) {
+    console.error('Database Connection Error: ' + err.stack);
+    return;
+  }
+
+  console.log('Database successfully connected');
+});
+
+
+module.exports = db;
